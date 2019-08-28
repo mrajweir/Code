6c3334c1247066bb2c354dbc3ebf0270d5d567ec
Andrew Weir <andrew@ajweir.co.uk>
2019-08-28 21:01:21 +0100

    Adding MrTumble unit tests - how exciting!

--- MrTumble/MrTumble.py
+++ MrTumble/MrTumble.py
@@ -1,18 +1,11 @@
 import time
 
 
-class Colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
 def banner():
+    """
+    Who doesn't love a bit of ASCII?
+    :return: Nothing
+    """
     print(" ")
     print("              _  _  ____       ____  _  _  ____  _  _  __    ____               ")
     print("             ( \/ )(  _ \     (_  _)/ )( \(  _ \( \/ )(  )  (  __)              ")
@@ -25,6 +18,12 @@ def banner():
 
 
 def luhn_checksum(card_number):
+    """
+    Compute the Luhn checksum algorithm on all digits of the card number, and return whether or not the card number
+    is constructed correctly.
+    :param card_number:  16-digit card number to verify
+    :return: True or False
+    """
     def digits_of(n):
         return [int(d) for d in str(n)]
     digits = digits_of(card_number)
@@ -38,10 +37,44 @@ def luhn_checksum(card_number):
 
 
 def is_luhn_valid(card_number):
+    """
+    Wrapper around the Luhn check.
+    :param card_number: 16-digit card number to verify
+    :return: True or False
+    """
     return luhn_checksum(card_number) == 0
 
 
-def main():
+def find_card(first_six, last_four):
+    """
+    Given the first six and last four digits of a 16-digit card number, find all possible valid card numbers.
+    :param first_six:  Six digits
+    :param last_four:  Four digits
+    :return: A list of dictionaries with the iteration and the card number.
+    """
+    cards_found = list()
+
+    # Replace 9999 with the number of digits between the first_six and last_four to make up a card number.
+    # IE: "400000" + i_in_9999 + "0002"
+    #
+    card_count = 0
+
+    for i in range(0, 9999):
+        # Get me a card number, prefixing the iteration with zeroes
+        card_number = first_six + "{:04d}".format(i) + last_four
+
+        # Was the number I generated valid in the first place?
+        if is_luhn_valid(card_number):
+            # Got a hit, increment the count
+            card_count = card_count + 1
+            # Print out how many card numbers we've found along with the card number
+            cards_found.append({"card_count": card_count, "card_number": card_number})
+
+    return cards_found
+
+
+# When run as a script, give the user something interesting to do.
+if __name__ == "__main__":
     banner()
 
     # Ask the user to enter in the card range they want to find
@@ -53,27 +86,13 @@ def main():
     before = time.time()
 
     # how many did it find?
-    card_count = 0
+    cards_found = find_card(first_six, last_four)
 
-    # Replace 9999 with the number of digits between the first_six and last_four to make up a card number.
-    # IE: "400000" + i_in_9999 + "0002"
-    #
-    for i in range(0, 9999):
-        # Get me a card number, prefixing the iteration with zeroes
-        card_number = first_six + "{:04d}".format(i) + last_four
-
-        # Was the number I generated valid in the first place?
-        if is_luhn_valid(card_number):
-            # Got a hit, increment the count
-            card_count = card_count + 1
-            # Print out how many card numbers we've found along with the card number
-            print("[{:-7d}] ".format(card_count) + card_number)
+    for d in cards_found:
+        print("[{:-7d}] ".format(d["card_count"]) + d["card_number"])
 
     # how long did it take to finish?
     after = time.time()
 
     # Total execution time was
     print("-- Mr. Tumble took [ {0} ] seconds.".format(after-before))
-
-
-main()
---
+++ MrTumble/tests/test_MrTumble.py
@@ -0,0 +1,36 @@
+import MrTumble
+import unittest
+
+class TestMrTumble(unittest.TestCase):
+
+    def test_doesLuhnDetectValidNumbers(self):
+        luhn_component = MrTumble.is_luhn_valid("400000000002")
+        self.assertEqual(luhn_component, True)
+
+    def test_doesLuhnDetectInvalidNumbers(self):
+        luhn_component = MrTumble.is_luhn_valid("4000000000003")
+        self.assertEqual(luhn_component, False)
+
+    def test_canFindCard(self):
+        # There has got to be a more elegant way to find if a value in a dictionary exists in a list - and I bet it
+        # involves sets or better structuring the data too.
+        output = MrTumble.find_card("400000", "0002")
+
+        for d in output:
+            if d["card_number"] == "40000000000002":
+                self.assertEqual(True, True)
+                return
+
+        self.assertEqual(True, False)
+
+    def test_cantFindCardIntentionally(self):
+        # There has got to be a more elegant way to find if a value in a dictionary exists in a list - and I bet it
+        # involves sets or better structuring the data too.
+        output = MrTumble.find_card("400000", "0002")
+
+        for d in output:
+            if d["card_number"].__eq__("4000000000000003"):
+                self.assertEqual(True, False)
+
+        # Remember! It shouldn't be able to find that PAN in the output
+        self.assertEqual(True, True)

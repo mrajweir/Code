0e0f68d64b2671fe0f9743fab9e562d98c80d028
Andrew Weir <andrew@ajweir.co.uk>
2026-01-18 18:10:04 +0000

    MoMath 2026 Mindbenders

---
+++ MoMath2026/January.py
@@ -0,0 +1,13 @@
+import math
+
+"""
+A ladybug lands on the 12 of a clock, and every second she moves randomly to a
+neighbouring number, either one step clockwise or counter-clockwise. 
+
+What is the probability that the very last number to be visited is 6?
+
+https://www.youtube.com/shorts/t3jZ2xGOvYg
+"""
+
+while True:
+    pass
